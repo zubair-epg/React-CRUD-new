3cd7f7ddb5f51ad59c26f8515a91563bc8d79489
zubair-epg <zubair.epaging@gmail.com>
2022-03-19 07:08:29 +0500

    semantic-ui dropdown working in update.js

--- src/components/update.js
+++ src/components/update.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form , Dropdown} from "semantic-ui-react";
 import axios from "axios";
 import { useHistory } from "react-router";
 
@@ -7,9 +7,19 @@ import { useHistory } from "react-router";
 export default function Update() {
   let history = useHistory();
   // const [id, setID] = useState(null);
-  const [nftId, setNftId] = useState(null);
+  const [nftId, setNftId] = useState([]);
    const [eventId, setEventId] = useState([]);
 
+   const nftIdApi = () => {
+    axios
+      .get(`http://10.194.2.113:2087/admin/nft/select`, {
+        headers: {
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Il9pZCI6IjYyMDNkZWIyOGQ4NGE5NDBmNDgzMGMyMyIsImFkZHJlc3MiOiIweDlhZEMyZUFEZTAxZUFDODdkMDVhMTY1ODkwZDY0MjQ5NTRFMGZjMjIiLCJzdGF0dXMiOmZhbHNlfSwiaWF0IjoxNjQ2MjQyNDA5LCJleHAiOjE2NDY4NDcyMDl9.LmYnNvgMrw5YXMyuTNYNNH-7aMRIFBpbKuE7fr-vdbU",
+        },
+      })
+      .then((response) => setNftId(response.data.data));
+  };
   useEffect(() => {
     // setID(localStorage.getItem("ID"));
     // setNftId(localStorage.getItem("Full Name"));
@@ -35,51 +45,103 @@ export default function Update() {
       });
   };
 
-  const nftIdApi = () => {
-    axios
-      .get(`http://10.194.2.113:2087/admin/nft/select`, {
-        headers: {
-          Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Il9pZCI6IjYyMDNkZWIyOGQ4NGE5NDBmNDgzMGMyMyIsImFkZHJlc3MiOiIweDlhZEMyZUFEZTAxZUFDODdkMDVhMTY1ODkwZDY0MjQ5NTRFMGZjMjIiLCJzdGF0dXMiOmZhbHNlfSwiaWF0IjoxNjQ2MjQyNDA5LCJleHAiOjE2NDY4NDcyMDl9.LmYnNvgMrw5YXMyuTNYNNH-7aMRIFBpbKuE7fr-vdbU",
-        },
-      })
-      .then((response) => setNftId(response.data));
-  };
-  console.log(typeof nftId);
-  console.log(nftIdApi);
-// const options = option
-  const options = [
-    { text: "Male", value: "male" },
-    { text: "Female", value: "female" },
-    { text: "Other", value: "other" },
-  ];
+  // const options = option
+  // console.log(nftIdApi);
+  const nftOptions = nftId.map(d => ({text:d.text , value:d.id}))
+   console.log(nftOptions);
+  const nftOptionId = nftId.map(d => ({value:d.id}))
+  var nftIdVal = nftOptionId.filter(singleOptions => (singleOptions.nftOptions));
+
+
+
+
+  const friendOptions = [
+    {
+      key: 'Jenny Hess',
+      text: 'Jenny Hess',
+      value: 'Jenny Hess',
+      image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
+    },
+    {
+      key: 'Elliot Fu',
+      text: 'Elliot Fu',
+      value: 'Elliot Fu',
+      image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
+    },
+    {
+      key: 'Stevie Feliciano',
+      text: 'Stevie Feliciano',
+      value: 'Stevie Feliciano',
+      image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
+    },
+    {
+      key: 'Christian',
+      text: 'Christian',
+      value: 'Christian',
+      image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
+    },
+    {
+      key: 'Matt',
+      text: 'Matt',
+      value: 'Matt',
+      image: { avatar: true, src: '/images/avatar/small/matt.jpg' },
+    },
+    {
+      key: 'Justen Kitsune',
+      text: 'Justen Kitsune',
+      value: 'Justen Kitsune',
+      image: { avatar: true, src: '/images/avatar/small/justen.jpg' },
+    },
+  ]
+  function selectedNftId() {
+    var nftIdNode = document.getElementById('subjectName');
+    var value = nftIdNode.options[nftIdNode.selectedIndex].value;
+    console.log("The selected value=" + value);
+ }
+  const NftOptionSelection = () => (
+    <Dropdown
+    id="subjectName"
+      placeholder='Select Friend'
+      fluid
+      selection
+      options={nftOptions}
+      onChange={selectedNftId}
+    />
+  )
+  const EventOptionSelection = () => (
+    <Dropdown
+      placeholder='Select Friend'
+      fluid
+      selection
+      options={friendOptions}
+      onAddItem={(e) => console.log(e.target.SelectedIndex)}
+    />
+  )
+
+
+
+  console.log(nftOptions);
+  console.log(nftOptionId);
   return (
     <div>
-      
+    
       <Form className="create-form">
         <Form.Field>
-          <Form.Select
+          {/* <Form.Select
             fluid
             label='NFT ID'
-            options={Object.keys(nftId).map((d) => d.nftId)}
-            placeholder='Gender'
-             onChange={(e) => setNftId(e.target.value)}
-          />
-    
+            options={nftOptions}
+            //  value={nftOptions}
+            placeholder='NFT ID'
+            //  onChange={(e) => setNftId(e.target.value)}
+          /> */}
+        <NftOptionSelection />
 
-          {/* <label>nft id</label>
-                    <input placeholder='nft id' onChange={(e) => setNftId(e.target.value)}/> */}
+    
         </Form.Field>
         <Form.Field>
-          <Form.Select
-            fluid
-            label="EVENT ID"
-            options={options}
-            placeholder="Gender"
-            onChange={(e) => e.target.value}
-          />
-          {/* <label>eventId</label>
-                    <input placeholder='event id'  onChange={(e) => setEventId(e.target.value)}/> */}
+        
+        <EventOptionSelection />
         </Form.Field>
         <Button type="submit" onClick={updateAPIData('6228e89efdab1e1e30e5f22b')}>
           Update
