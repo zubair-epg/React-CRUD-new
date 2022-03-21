7414a382fbe8ff3b0cd19c8b1ced1fce5bb8c0ef
zubair-epg <zubair.epaging@gmail.com>
2022-03-22 02:24:02 +0500

    working-continue

--- src/components/update.js
+++ src/components/update.js
@@ -5,32 +5,45 @@ import { useHistory } from "react-router";
 
 export default function Update() {
   let history = useHistory();
-  // const [id, setID] = useState(null);
+  
+  const [nftIdVal, setNftIdVal] = useState('');
   const [nftId, setNftId] = useState([]);
-   const [eventId, setEventId] = useState([]);
+  const [eventId, setEventId] = useState([]);
+const ticketId = localStorage.getItem("Id")
+
    
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Il9pZCI6IjYyMDNkZWIyOGQ4NGE5NDBmNDgzMGMyMyIsImFkZHJlc3MiOiIweDlhZGMyZWFkZTAxZWFjODdkMDVhMTY1ODkwZDY0MjQ5NTRlMGZjMjIiLCJzdGF0dXMiOmZhbHNlfSwiaWF0IjoxNjQ3NjI2NDAyLCJleHAiOjE2NDgyMzEyMDJ9.VM72QD2nEPmJ-LiFSfEaIIveOyP2aOoG5zpUULSc19k'
    const nftIdApi = () => {
     axios
       .get(`http://10.194.2.113:2087/admin/nft/select`, {
         headers: {
           Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Il9pZCI6IjYyMDNkZWIyOGQ4NGE5NDBmNDgzMGMyMyIsImFkZHJlc3MiOiIweDlhZEMyZUFEZTAxZUFDODdkMDVhMTY1ODkwZDY0MjQ5NTRFMGZjMjIiLCJzdGF0dXMiOmZhbHNlfSwiaWF0IjoxNjQ2MjQyNDA5LCJleHAiOjE2NDY4NDcyMDl9.LmYnNvgMrw5YXMyuTNYNNH-7aMRIFBpbKuE7fr-vdbU",
+            `${token}`,
         },
       })
       .then((response) => setNftId(response.data.data));
   };
-  useEffect(() => {
-    // setID(localStorage.getItem("ID"));
-    // setNftId(localStorage.getItem("Full Name"));
-    // setEventId(localStorage.getItem("Age"));
 
-     nftIdApi();
-  }, []);
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Il9pZCI6IjYyMDNkZWIyOGQ4NGE5NDBmNDgzMGMyMyIsImFkZHJlc3MiOiIweDlhZGMyZWFkZTAxZWFjODdkMDVhMTY1ODkwZDY0MjQ5NTRlMGZjMjIiLCJzdGF0dXMiOmZhbHNlfSwiaWF0IjoxNjQ3NjI2NDAyLCJleHAiOjE2NDgyMzEyMDJ9.VM72QD2nEPmJ-LiFSfEaIIveOyP2aOoG5zpUULSc19k'
-
-  const updateAPIData = (id) => {
+  const eventIdApi = () => {
     axios
-      .put(`http://10.194.2.113:2087/api/v1/metaverse/ticket/${id}`, {
+      .get(`http://10.194.2.113:2087/api/v1/metaverse/event`, {
+        headers: {
+          Authorization:
+            `${token}`,
+        },
+      })
+      .then((response) => setEventId(response.data.data));
+  };
+  useEffect(() => {
+  
+     nftIdApi();
+     eventIdApi();
+  }, []);
+
+  const updateAPIData = ( ) => {
+    console.log('hitted')
+    axios
+      .put(`http://10.194.2.113:2087/api/v1/metaverse/ticket/${ticketId}`, {
         nftId,
         eventId
       },
@@ -44,100 +57,51 @@ export default function Update() {
       });
   };
 
-  // const options = option
-  // console.log(nftIdApi);
   const nftOptions = nftId.map(d => ({text:d.text , value:d.id}))
-   console.log(nftOptions);
-  const nftOptionId = nftId.map(d => ({value:d.id}))
-  var nftIdVal = nftOptionId.filter(singleOptions => (singleOptions.nftOptions));
-  const friendOptions = [
-    {
-      key: 'Jenny Hess',
-      text: 'Jenny Hess',
-      value: 'Jenny Hess',
-      image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
-    },
-    {
-      key: 'Elliot Fu',
-      text: 'Elliot Fu',
-      value: 'Elliot Fu',
-      image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
-    },
-    {
-      key: 'Stevie Feliciano',
-      text: 'Stevie Feliciano',
-      value: 'Stevie Feliciano',
-      image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
-    },
-    {
-      key: 'Christian',
-      text: 'Christian',
-      value: 'Christian',
-      image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
-    },
-    {
-      key: 'Matt',
-      text: 'Matt',
-      value: 'Matt',
-      image: { avatar: true, src: '/images/avatar/small/matt.jpg' },
-    },
-    {
-      key: 'Justen Kitsune',
-      text: 'Justen Kitsune',
-      value: 'Justen Kitsune',
-      image: { avatar: true, src: '/images/avatar/small/justen.jpg' },
-    },
-  ]
-  function selectedNftId() {
-    var nftIdNode = document.getElementById('subjectName');
-    var value = nftIdNode.options[nftIdNode.selectedIndex].value;
-    console.log("The selected value=" + value);
- }
+  const eventOptions = nftId.map(d => ({text:d.text , value:d.id}))
+
+  const nftDropDownSelect = (event , data) => {
+    setNftIdVal(data.value)
+    console.log(nftIdVal)
+    console.log(event)
+  };
+  //  const eventDropDownSelect = (event , data) => {
+  //   setEventIdVal(data.value)
+  //   console.log(nftIdVal)
+  //  };
+
   const NftOptionSelection = () => (
     <Dropdown
-    id="subjectName"
+    
       placeholder='Select Nft Id'
+      options={nftOptions}
+      onChange={nftDropDownSelect}
       fluid
       selection
-      options={nftOptions}
-      onChange={selectedNftId}
-    />
+      />
   )
   const EventOptionSelection = () => (
     <Dropdown
+    id="subjectName"
       placeholder='Select Friend'
       fluid
       selection
-      options={friendOptions}
-      onAddItem={selectedNftId}
+      options={eventOptions}
+      // onChange={eventDropDownSelect}
     />
   )
 
-
-
-  console.log(nftOptions);
-  console.log(nftOptionId);
   return (
     <div>
     
       <Form className="create-form">
         <Form.Field>
-          {/* <Form.Select
-            fluid
-            label='NFT ID'
-            options={nftOptions}
-            //  value={nftOptions}
-            placeholder='NFT ID'
-            //  onChange={(e) => setNftId(e.target.value)}
-          /> */}
-        <NftOptionSelection />
-
+        <NftOptionSelection  />
         </Form.Field>
         <Form.Field>
-        
         <EventOptionSelection />
         </Form.Field>
-        <Button type="submit" onClick={updateAPIData('6228e89efdab1e1e30e5f22b')}>
+        <Button type="submit" onClick={()=> updateAPIData(nftIdVal)}>
           Update
         </Button>
       </Form>
